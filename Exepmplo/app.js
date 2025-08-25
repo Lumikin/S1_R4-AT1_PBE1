7df7c3955d8f413e033dc0075588451b2e0ba14c
Lumikin <lumikomio04@gmail.com>
2025-08-25 16:13:50 -0300

    Arrumando os arquivos

---
+++ Exepmplo/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 8081 // Porta padrão 
+
+app.get('/hello/',(req, res)=>{
+   try {
+        const {nome, idade} = req.query;
+       
+
+     if (nome == undefined || idade == undefined || isNaN(idade)){
+        return res.status(400).send(`dados obrigatorios não preenchidos`);
+     }
+
+        res.status(200).send(`Ola, ${nome} Você tem a ${idade} anos`)
+
+   } catch (error) {
+    
+    console.error(`Erro ao dizer olá: ${error}`)
+    res.status(500).send(`erro interno do servidor!`)
+
+   }
+   
+})
+
+// ultima linha do codigo
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
