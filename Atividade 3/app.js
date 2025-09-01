2c3fad111dd1e6167c0a59847318e2ce09853a3f
Lumikin <lumikomio04@gmail.com>
2025-09-01 10:48:31 -0300

    Arrumando a atividade 3

--- Atividade 3/app.js
+++ Atividade 3/app.js
@@ -3,47 +3,30 @@ const express = require('express');
 const app = express();
 const PORT = 8081
 
-app.get('/operacao/:tipo/', (req, res) => {
-try {
-    
-    
-    //     const { num1, num2 } = req.query;
-    //     let resultado = parseFloat(num1) + parseFloat(num2) //Soma
-    //     if (num1 == undefined || num2 == undefined || isNaN(num1, num2)) {
-    //         return res.status(400).send(`Os numeros não estao sendo reconhecidos!`)
-    //     }
+app.get('/operacao/:tipo/:num1/:num2/', (req, res) => {
+    try {
+
+        let resAdicao
+        let resSubtracao
+
+        const { tipo } = req.query.tipo;
+        const { num1 } = req.query.num1;
+        const { num2 } = req.query.num2;
 
 
-    //     res.status(200).send(`A adição é ${resultado}`)
+        switch (tipo) {
+            case soma:
+                resAdicao = parseFloat(num1) + parseFloat(num2)
+                res.status(200).send(`Ò resultado é ${resAdicao}`)
+                break;
 
+            default:
+                break;
+        }
+    }catch (erro){
 
-    // } catch (erro) {
-    //     console.error(`O erro: ${erro}`)
-    //     res.status(500).send(`Erro interno do servidor`)
-    // }
-
-
-})
-
-// app.get('/operacao/:subtracao/', (req, res) => {
-// try {
-//         const { num1, num2 } = req.query;
-//         let resultado = parseFloat(num1) - parseFloat(num2) //Subtracao
-//         if (num1 == undefined || num2 == undefined || isNaN(num1, num2)) {
-//             return res.status(400).send(`Os numeros não estao sendo reconhecidos!`)
-//         }
-
-
-//         res.status(200).send(`A subtração é ${resultado}`)
-
-
-//     } catch (erro) {
-//         console.error(`O erro: ${erro}`)
-//         res.status(500).send(`Erro interno do servidor!`)
-//     }
-
-
-// })
+    }
+    })
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
