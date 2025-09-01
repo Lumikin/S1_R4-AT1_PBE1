df90c278056d462824a88c88d5cf999f660e56e3
Lumikin <lumikomio04@gmail.com>
2025-09-01 11:06:57 -0300

    finalizado a atividade 3

--- Atividade 3/app.js
+++ Atividade 3/app.js
@@ -3,31 +3,37 @@ const express = require('express');
 const app = express();
 const PORT = 8081
 
-app.get('/operacao/:tipo/:num1/:num2/', (req, res) => {
+app.get('/operacao/:tipo/', (req, res) => {
     try {
 
-        let resAdicao
-        let resSubtracao
 
-        const { tipo } = req.query.tipo;
-        const { num1 } = req.query.num1;
-        const { num2 } = req.query.num2;
+        const { tipo } = req.params;
+        const { num1 } = req.query;
+        const { num2 } = req.query;
 
 
         switch (tipo) {
-            case soma:
-                resAdicao = parseFloat(num1) + parseFloat(num2)
-                res.status(200).send(`Ò resultado é ${resAdicao}`)
+            case 'soma':
+                resultado = parseFloat(num1) + parseFloat(num2)
+                break;
+            case 'subtracao':
+                resultado = parseFloat(num1) - parseFloat(num2)
+                break;
+            case 'multiplicacao':
+                resultado = parseFloat(num1) * parseFloat(num2)
+                break;
+            case 'divisao':
+                resultado = parseFloat(num1) / parseFloat(num2)
                 break;
 
             default:
-                break;
+            return res.status(200).send(`Operacao Invalida`)
         }
-    }catch (erro){
+        return res.status(200).send(`A ${tipo} é ${resultado}`)
+    } catch (erro) {
 
     }
-    })
-
+})
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
